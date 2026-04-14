67640f92717017f8f48bfbca117c158b7ce04316
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-04-14 20:16:40 -0300

    header paginas exceto rastrear

--- frontend/src/pages/PromotionsPage/PromotionsPage.tsx
+++ frontend/src/pages/PromotionsPage/PromotionsPage.tsx
@@ -1,10 +1,12 @@
 import CatalogPage from "../CatalogPage/CatalogPage"
+import heroPromotions from "../../assets/images/heroes/hero-promotions.png"
 
 function PromotionsPage() {
   return (
     <CatalogPage
       title="Promoções"
       subtitle="Ofertas exclusivas por tempo limitado."
+      heroImage={heroPromotions}
     />
   )
 }
